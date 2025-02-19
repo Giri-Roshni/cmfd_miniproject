f69bd4d6536a2c9a8e10a37bdc4735364d24b138
Roshni Giri <roshanigiri82@gmail.com>
2025-02-19 14:23:06 +0545

    resizing image

---
+++ detect.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+from sklearn.cluster import DBSCAN
+
+class Detect:
+    def __init__(self, image):
+        self.image = image
+    
+    def siftDetector(self):
+        sift = cv2.SIFT_create()
+        gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        self.key_points, self.descriptors = sift.detectAndCompute(gray, None)
+        return self.key_points, self.descriptors
+    
+    def locateForgery(self, eps=40, min_sample=2):
+        clusters = DBSCAN(eps=eps, min_samples=min_sample).fit(self.descriptors)
+        size = np.unique(clusters.labels_).shape[0] - 1
+        forgery = self.image.copy()
+        non_forgery = self.image.copy()
+        
+        if size == 0 and np.unique(clusters.labels_)[0] == -1:
+            return None, None, None
+        
+        size = max(size, 1)
+        cluster_list = [[] for _ in range(size)]
+        for idx, label in enumerate(clusters.labels_):
+            if label != -1:
+                cluster_list[label].append(
+                    (int(self.key_points[idx].pt[0]), int(self.key_points[idx].pt[1]))
+                )
+        
+        forgery_parts = []
+        for points in cluster_list:
+            if len(points) > 1:
+                forgery_parts.append(points)
+                for idx1 in range(1, len(points)):
+                    cv2.line(forgery, points[0], points[idx1], (0, 255, 0), 2)
+                    cv2.circle(non_forgery, points[idx1], 5, (0, 0, 255), -1)
+                    cv2.putText(forgery, "Copied", points[0], cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+                    cv2.putText(non_forgery, "Original", points[idx1], cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        
+        return forgery, non_forgery, forgery_parts
---
+++ utils.py
@@ -0,0 +1,16 @@
+import cv2
+from PIL import Image, ImageTk
+
+def resize_image(image, max_width=800, max_height=400):
+    h, w, _ = image.shape
+    scale_factor = min(max_width / w, max_height / h, 1)
+    if scale_factor < 1:
+        image = cv2.resize(image, (int(w * scale_factor), int(h * scale_factor)))
+    return image
+
+def display_image(image, label):
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image_pil = Image.fromarray(image_rgb)
+    image_tk = ImageTk.PhotoImage(image_pil)
+    label.config(image=image_tk)
+    label.image = image_tk
