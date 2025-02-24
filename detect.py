6adae0e0ecec7dbad7f2d01b1251e1338c439c7b
Roshni Giri <roshanigiri82@gmail.com>
2025-02-24 14:52:52 +0545

    eps

--- detect.py
+++ detect.py
@@ -15,9 +15,9 @@ class Detect:
         self.key_points, self.descriptors = sift.detectAndCompute(gray, None)
         return self.key_points, self.descriptors
     
-    def locateForgery(self, eps=40, min_sample=2):
+    def locateForgery(self, eps=36, min_sample=2):
         clusters = DBSCAN(eps=eps, min_samples=min_sample).fit(self.descriptors)
-        size = np.unique(clusters.labels_).shape[0] - 1
+        size = np.unique(clusters.labels_).shape[0]-1 
         forgery = self.image.copy()
         non_forgery = self.image.copy()
         
@@ -43,10 +43,10 @@ class Detect:
                 forgery_parts.append(points)
                 for idx1 in range(1, len(points)):
                     cv2.line(forgery, points[0], points[idx1], (200, 120, 100), 2)
-                    cv2.circle(non_forgery, points[idx1], 5, (0, 0, 255), -1)
-                    cv2.putText(forgery, "Copied", points[0], cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
-                    cv2.putText(forgery, "Original", points[idx1], cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-                    cv2.putText(non_forgery, "Original", points[idx1], cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+                    # cv2.circle(non_forgery, points[idx1], 5, (0, 0, 255), -1)
+                    # cv2.putText(forgery, "Copied", points[0], cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+                    # cv2.putText(forgery, "Original", points[idx1], cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+                    # cv2.putText(non_forgery, "Original", points[idx1], cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
                     
                     cv2.circle(binary_mask, points[0], 10, 255, -1)
                     cv2.circle(binary_mask, points[idx1], 10, 255, -1)
